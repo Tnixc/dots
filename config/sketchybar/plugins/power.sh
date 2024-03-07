d62fbc3ea3c903c6ebd38dddb9954b902121e12a
Tnixc <tnixxc@gmail.com>
2024-03-07 10:59:33 -0500

    update sketchybar to use SF symbols

--- config/sketchybar/plugins/power.sh
+++ config/sketchybar/plugins/power.sh
@@ -8,19 +8,19 @@ if [ $PERCENTAGE = "" ]; then
 fi
 
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="􀛨"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="􀺸"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="􀺶"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="􀛩"
   ;;
   *) ICON=""
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=""
+  ICON="􀢋"
 fi
 
 sketchybar --set power_logo icon="$ICON" --set battery label="${PERCENTAGE}%"
