d8c067af0113917d9592838f5724a1f5f0e1699f
Tnixc <tnixxc@gmail.com>
2024-03-15 14:42:25 -0400

    lvim

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -2,8 +2,6 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/ Discord: https://discord.com/invite/Xb9B4Ny
 
--- require("emmet")
-
 lvim.plugins = {
   { "ThePrimeagen/vim-be-good" },
   { "pocco81/auto-save.nvim" },
@@ -34,7 +32,7 @@ lvim.plugins = {
       require("copilot").setup({
         panel = {
           enabled = true,
-          auto_refresh = false,
+          auto_refresh = true,
           keymap = {
             jump_prev = "[[",
             jump_next = "]]",
