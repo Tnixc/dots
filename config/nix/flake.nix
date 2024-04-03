4b377a335b79f58d305cbed2890762bbe476fa6d
Tnixc <tnixxc@gmail.com>
2024-04-03 13:18:27 -0400

    nix things?

--- config/nix/flake.nix
+++ config/nix/flake.nix
@@ -18,7 +18,7 @@
       nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
+      # programs.zsh.enable = true;  # default shell on catalina
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
